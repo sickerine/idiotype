c67f8181a80185b9140efafee0b1d933e733c700
Seikirin <seikirin.tempest@gmail.com>
2024-04-21 20:35:53 +0100

    basic ui and socket

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,167 @@
+"use client";
 import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+import { twMerge } from "tailwind-merge";
+import { Lock } from "lucide-react";
+import { socket } from "@/socket/socket"
 
 export default function Home() {
+  const sentence = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero ad rem ea voluptatibus excepturi blanditiis laborum necessitatibus ipsum animi quaerat placeat fuga sapiente maiores natus, culpa commodi itaque provident suscipit?"
+  const words = sentence.split(" ");
+  const [focused, setFocused] = useState(true);
+  const [typedText, setTypedText] = useState("");
+  const [capsLock, setCapsLock] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const cursorRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function onConnect() {
+      console.log("connected");
+    }
+
+    function onDisconnect() {
+      console.log("disconnected");
+    }
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+    socket.on("new-text", (text: string) => {
+      console.log("new text", text);
+    })
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+    }
+  }, [])
+
+
+  useEffect(() => {
+    const updateCursor = () => {
+      if (cursorRef.current) {
+        const span = document.getElementById(String(typedText.length));
+        const previousSpan = document.getElementById(String(typedText.length - 1));
+        console.log("found span", span)
+        if (span) {
+          cursorRef.current.style.left = span.offsetLeft + "px";
+          cursorRef.current.style.top = span.offsetTop + span.offsetHeight / 2 + "px";
+          cursorRef.current.style.opacity = "1";
+        } else if (previousSpan) {
+          cursorRef.current.style.left = previousSpan.offsetLeft + previousSpan.offsetWidth + "px";
+          cursorRef.current.style.top = previousSpan.offsetTop + previousSpan.offsetHeight / 2 + "px";
+          cursorRef.current.style.opacity = "1";
+
+        }
+      }
+    }
+
+
+    const listener = (e: KeyboardEvent) => {
+      if (!focused) return;
+
+      const alphabetKeys = "abcdefghijklmnopqrstuvwxyz";
+      const capitalAlphabetKeys = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      const punctuationKeys = ".,?!;:'\"(){}[]";
+      const spaceKey = " ";
+
+      console.log(e.key);
+
+      if (e.key === "Backspace") {
+        setTypedText((prev) => {
+          const newText = prev.slice(0, prev.length - 1);
+          console.log("current text", prev);
+          console.log("new text", newText);
+          return newText;
+        })
+      } else if ((alphabetKeys
+        + capitalAlphabetKeys
+        + punctuationKeys
+        + spaceKey).includes(e.key)) {
+        setTypedText((prev) => prev + e.key);
+      }
+
+      socket.emit("typing", e.key)
+
+      updateCursor();
+    }
+
+    const capsLockListener = (e: KeyboardEvent) => {
+      setCapsLock(e.getModifierState("CapsLock"));
+    }
+
+    window.addEventListener("keydown", listener);
+    window.addEventListener("keydown", capsLockListener);
+    window.addEventListener("keyup", capsLockListener);
+    updateCursor();
+    return () => {
+      window.removeEventListener("keydown", listener);
+      window.removeEventListener("keydown", capsLockListener);
+      window.removeEventListener("keyup", capsLockListener);
+    }
+  }, [focused, typedText])
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex justify-center items-center h-screen w-screen bg-sky-950/50 flex-col gap-4">
+      <div className="flex flex-col gap-4">
+        <div className="flex w-full items-end gap-2">
+          <div className="text-[3rem] leading-[3rem] font-black">
+            0
+          </div>
+          <div className="text-sm">
+            WPM
+          </div>
+          <div className="text-sm ml-auto">
+            Accuracy
+          </div>
+          <div className="text-[3rem] leading-[3rem] font-black">
+            100
+          </div>
+          <div>
+            %
+          </div>
+        </div>
+        <div className={"w-full max-w-[1280px] bg-sky-950 rounded-3xl overflow-hidden relative"}>
+          <div className={twMerge("absolute  inset-0 flex justify-center items-center font-medium text-xl opacity-100 transition-all", focused && "opacity-0 -translate-x-24")}>
+            Focus to start typing.
+          </div>
+          <div className={twMerge("blur-lg relative transition-all", focused && "blur-none")}>
+            <input ref={inputRef} autoFocus onBlur={(e) => setFocused(false)} onFocus={(e) => setFocused(true)} className="absolute w-full h-full inset-0 z-10 opacity-0" />
+            <div className="flex flex-wrap content-start text-xl gap-2 p-6 relative">
+              <div ref={cursorRef} className="absolute w-[1px] h-6 bg-white opacity-0 transition-all delay-0 -translate-y-1/2" />
+              {
+                words.map((word, wordIndex) => {
+                  const letters = word.split("");
+                  return (
+                    <div key={wordIndex} className="flex bg-white/10 p-2 rounded-lg">
+                      {
+                        letters.map((letter, letterIndex) => {
+                          const actualIndex = words.slice(0, wordIndex).join(" ").length + (wordIndex > 0 ? 1 : 0) + letterIndex
+                          return (
+                            <span id={String(actualIndex)} key={letterIndex} className={twMerge("text-white",
+                              typedText.length - 1 < actualIndex && "text-opacity-50",
+                              (typedText.length > actualIndex && typedText[actualIndex] !== letter) && "text-red-500",
+                            )}>{letter}</span>
+                          )
+                        })
+                      }
+                    </div>
+                  )
+                })
+              }
+            </div>
+            <div className={twMerge("w-full h-0 bg-sky-600 transition-all flex justify-center items-center overflow-hidden text-xs gap-1", capsLock && "h-6")} >
+              <Lock size={12} />
+              Caps Lock
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <button className="px-4 py-2 rounded-xl active:bg-white/10 active:scale-95 transition-all focus:bg-white/10 focus:outline-none" onClick={() => {
+        setTypedText("");
+        inputRef.current?.focus();
+      }}>
+        Retry
+      </button>
     </main>
   );
 }
---
+++ server/index.ts
@@ -0,0 +1,32 @@
+const { createServer } = require("http");
+const next = require("next");
+const { Server } = require("socket.io");
+
+const dev = process.env.NODE_ENV !== "production";
+const hostname = "localhost";
+const port = 3000;
+const app = next({ dev, hostname, port });
+const handler = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const httpServer = createServer(handler);
+
+  const io = new Server(httpServer);
+
+  io.on("connection", (socket: any) => {
+    console.log("connected", socket.id);
+    socket.on("typing", (text: string) => {
+      console.log("new text", text);
+      socket.emit("new-text", text);
+    });
+  });
+
+  httpServer
+    .once("error", (err: any) => {
+      console.error(err);
+      process.exit(1);
+    })
+    .listen(port, () => {
+      console.log(`> Ready on http://${hostname}:${port}`);
+    });
+});
